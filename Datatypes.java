f55bd79d10301671507449a9c8a5bfe165ecfdd2
T.Ruchitha <thumuruchi@gmail.com>
2026-08-20 18:25:45 +0530

    Add DataTypes class with main method

---
+++ Datatypes.java
@@ -0,0 +1,17 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        int number = 100;
+        float price = 10.5f;
+        double salary = 99.99;
+        char grade = 'A';
+        boolean status = true;
+
+        System.out.println("Integer: " + number);
+        System.out.println("Float: " + price);
+        System.out.println("Double: " + salary);
+        System.out.println("Character: " + grade);
+        System.out.println("Boolean: " + status);
+    }
+}
